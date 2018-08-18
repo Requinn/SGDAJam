a6259794e781d2d89c6e9e304b48cdc34e88dd47
Fierfek <geekssw@gmail.com>
2018-08-18 15:32:18 -0700

    Nodes and Balls
    # Added Cannon Balls with trigger
    # Added Damageable Critical Node

---
+++ SGDAJam/Assets/_Scripts/DeathEvent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using MichaelWolfGames.DamageSystem;
+
+public class DeathEvent :  HealthManagerEventListenerBase {
+
+	// Use this for initialization
+	void Start() {
+
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	protected override void DoOnDeath() {
+		Debug.Log(this.name  + " has died!");
+	}
+
+	protected override void DoOnRevive() {
+		//throw new System.NotImplementedException();
+	}
+
+	protected override void DoOnTakeDamage(object sender, Damage.DamageEventArgs damageEventArgs) {
+		//throw new System.NotImplementedException();
+	}
+}
