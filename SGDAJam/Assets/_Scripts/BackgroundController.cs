c3fe115de228ebe9910ba685753541fe126f53b5
Joel Lee <lee.joel395@gmail.com>
2018-08-18 14:22:19 -0700

    Added in a controller fro the background, added a basic handler for the dragon

---
+++ SGDAJam/Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Controls the background of the environment
+/// </summary>
+public class BackgroundController : MonoBehaviour {
+    //DragonClass reference
+    [SerializeField]
+    private GameObject _backgroundObject; //object to move
+    private Vector3 _homePosition; //fake const value for the home position
+    private Vector3 _currentVelocity = Vector3.zero; //the current movement
+
+    private Vector3 _currentRotational; //the current rotational movement being added
+    private bool canMove = false;
+
+    public static BackgroundController BGInstance;
+
+    [Header("TEST PROPERTIES")]
+    public Vector3 velocity;
+    public float rotation;
+
+	// Use this for initialization
+	void Start () {
+        _homePosition = _backgroundObject.transform.position;
+	}
+
+    /// <summary>
+    /// Tell the background to move in a di
+    /// </summary>
+    /// <param name="direction"></param>
+    /// <param name="speed"></param>
+    public void ModifyScrollVelocty(Vector3 direction, float speed) {
+        _currentVelocity += direction * speed;
+    }
+
+    /// <summary>
+    /// Tell the background to rotate
+    /// </summary>
+    /// <param name="rotation"></param>
+    /// <param name="speed"> how many degrees per frame? </param>
+    public void AddRotation(float rotation, float speed){
+        _currentRotational += new Vector3(0, 0, rotation);
+    }
+
+    /// <summary>
+    /// reset the scrolling speed to default
+    /// </summary>
+    public void ResetScrollVelocity() {
+        _currentVelocity = Vector3.zero;
+    }
+
+    /// <summary>
+    /// reset the rotation speed to default
+    /// </summary>
+    public void ResetRotationVelocity() {
+        _currentRotational = Vector3.zero;
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.G)) {
+            ModifyScrollVelocty(velocity, 1);
+        }
+        if (Input.GetKeyDown(KeyCode.H)) {
+            AddRotation(rotation, 1);
+        }
+        //lerp the movement
+        _backgroundObject.transform.position = Vector3.Lerp(_backgroundObject.transform.position, _backgroundObject.transform.position + _currentVelocity, Time.deltaTime);
+        //lerp the rotation and convert back to quaternion
+        _backgroundObject.transform.rotation = Quaternion.Euler(Vector3.Lerp(_backgroundObject.transform.rotation.eulerAngles, _backgroundObject.transform.rotation.eulerAngles + _currentRotational, Time.deltaTime));
+    }
+
+    
+}
---
+++ SGDAJam/Assets/_Scripts/DragonHandler.cs
@@ -0,0 +1,30 @@
+﻿using MichaelWolfGames.DamageSystem;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class DragonHandler : MonoBehaviour {
+    [SerializeField]
+    HealthManagerBase[] _damageableNode;
+
+    // Use this for initialization
+    void Start() {
+        //subscribe to all the damageablenodes
+        for (int i = 0; i < _damageableNode.Length; i++) {
+            _damageableNode[i].OnTakeDamage += ReactToDamage;
+        }
+	}
+
+    /// <summary>
+    /// How will we handle the damage being dealt to us?
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="args"></param>
+    private void ReactToDamage(object sender, Damage.DamageEventArgs args) {
+        Debug.Log("Damaged with severity: " + args.DamageValue);
+        //when struck, speed up the animation of the dragon by X amount for some seconds
+    }
+
+
+}
