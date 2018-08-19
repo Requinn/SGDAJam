15e98937e956ce1b04f96d142858bbe5a3b33304
andresg6 <andresg6@uci.edu>
2018-08-18 22:53:26 -0700

    GameOver scene

---
+++ SGDAJam/Assets/_Scripts/UI/DumbSceneWorkaround.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DumbSceneWorkaround : MonoBehaviour {
+    //This is bad and I should feel bad
+    public void StartGame()
+    {
+        GameSceneManager.Instance.StartGame();
+    }
+}
