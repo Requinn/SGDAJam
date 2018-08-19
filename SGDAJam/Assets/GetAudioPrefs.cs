0cef367fce8f67cd353d6926d0b3a107c9680960
Fierfek <geekssw@gmail.com>
2018-08-19 12:57:18 -0700

    Music!!!
    # Tons of music and maybe some fx

---
+++ SGDAJam/Assets/GetAudioPrefs.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetAudioPrefs : MonoBehaviour {
+
+	AudioSource audio;
+	public float volumeModifier = 1f;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		audio.volume = PlayerPrefs.GetFloat("volume") * volumeModifier;
+
+		if(PlayerPrefs.GetFloat("mute") == 0f) {
+			audio.mute = false;
+		} else {
+			audio.mute = true;
+		}
+	}
+}
---
+++ SGDAJam/Assets/ManageVolume.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManageVolume : MonoBehaviour {
+
+	public AudioSource audio;
+	public ChangeImage ci;
+	public Slider volumeSlider;
+
+	// Use this for initialization
+	void Start () {
+		volumeSlider = GetComponent<Slider>();
+		volumeSlider.minValue = 0f;
+		volumeSlider.maxValue = 1f;
+		volumeSlider.value = 1f;
+
+		PlayerPrefs.SetFloat("volume", volumeSlider.value);
+	}
+
+	public void VolumeChange() {
+		audio.volume = volumeSlider.value;
+		PlayerPrefs.SetFloat("volume", volumeSlider.value);
+	}
+}
