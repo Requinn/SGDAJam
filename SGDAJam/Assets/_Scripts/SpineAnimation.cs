0ea54605e2e65c7f01d2423330f70ac542ac8e1d
Michaelwolf95 <michaelwolf95@gmail.com>
2018-08-19 12:22:45 -0700

    Dragon and anim stuff

--- SGDAJam/Assets/_Scripts/SpineAnimation.cs
+++ SGDAJam/Assets/_Scripts/SpineAnimation.cs
@@ -5,8 +5,13 @@ using UnityEngine;
 
 public class SpineAnimation : StateMachineBehaviour
 {
+    public string animationName;
+    public float speed = 1f;
+    public bool loop = true;
+
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        //SkeletonAnimation
+        SkeletonAnimation anim = animator.GetComponent<SkeletonAnimation>();
+        anim.state.SetAnimation(0, animationName, loop).timeScale = speed;
     }
 }
