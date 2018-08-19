517dd99fbc956432755fd033d45a592e81fb179e
Fierfek <geekssw@gmail.com>
2018-08-19 16:25:25 -0700

    New Music

---
+++ SGDAJam/Assets/MusicSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour {
+
+	AudioSource audio;
+	public AudioClip looper;
+
+	// Use this for initialization
+	void Start () {
+		audio = GetComponent<AudioSource>();
+		audio.PlayScheduled(AudioSettings.dspTime + audio.clip.length - audio.time);
+	}
+}
