54a02b27756b2e62cf326d2f92b80a33ac126a23
Joel Lee <lee.joel395@gmail.com>
2018-08-18 19:16:50 -0700

    Added in a sequence handler for the intro cutscene. Added in a fade in animation.

---
+++ SGDAJam/Assets/_Scripts/Sequence/SequencePlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SequencePlayer : MonoBehaviour {
+    public SequencObject[] steps;
+    int _playedIndex = 0; 
+	void Start () {
+        StartCoroutine(PlaySequence());
+	}
+
+    private IEnumerator PlaySequence() {
+        while (_playedIndex < steps.Length) {
+            steps[_playedIndex].PlaySequenceObject();
+            yield return new WaitForSeconds(steps[_playedIndex].sequenceDuration);
+            _playedIndex++;
+        }
+
+    }
+}
