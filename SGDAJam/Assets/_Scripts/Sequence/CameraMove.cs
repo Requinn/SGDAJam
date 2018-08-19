ffada4d94f3e92cd586a964beb299cc4376a4728
Joel Lee <lee.joel395@gmail.com>
2018-08-18 21:51:06 -0700

    Cloud arts + visual touches/effects. Modified the sequences for better quality.

--- SGDAJam/Assets/_Scripts/Sequence/CameraMove.cs
+++ SGDAJam/Assets/_Scripts/Sequence/CameraMove.cs
@@ -6,33 +6,9 @@ using UnityEngine;
 public class CameraMove : SequencObject {
     public Cinemachine.CinemachineVirtualCamera vCam;
     public Cinemachine.CinemachineVirtualCamera vCam2;
-    public float orthographSizeStart; //starting value of camera zoom
-    public float orthographSizeEnd; //ending value of camera zoom
 
     public override void PlaySequenceObject() {
-        //StartCoroutine(MoveAndAdjustCamera());
         vCam2.Priority = 0;
         vCam.Priority = 11;
     }
-
-    private IEnumerator MoveAndAdjustCamera() {
-        while (vCam.m_Lens.OrthographicSize > orthographSizeEnd) {
-            //vCam.m_Lens.OrthographicSize = Mathf.Lerp(vCam.m_Lens.OrthographicSize, vCam.m_Lens.OrthographicSize -= vCam.m_Lens.OrthographicSize * 0.45f, Time.deltaTime);
-            yield return new WaitForFixedUpdate();
-            Debug.Log(vCam.m_Lens.OrthographicSize);
-        }
-        vCam.m_Lens.OrthographicSize = orthographSizeEnd;
-    }
-
-    // Use this for initialization
-    void Start () {
-        //vCam.m_Lens.OrthographicSize = orthographSizeStart;
-        //PlaySequenceObject();
-
-    }
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
--- SGDAJam/Assets/_Scripts/Sequence/SequencePlayer.cs
+++ SGDAJam/Assets/_Scripts/Sequence/SequencePlayer.cs
@@ -1,21 +1,30 @@
-﻿using System;
+﻿using MichaelWolfGames;
+using MichaelWolfGames.CC2D;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class SequencePlayer : MonoBehaviour {
     public SequencObject[] steps;
+    private CharacterController2D _playerRef;
     int _playedIndex = 0; 
+
 	void Start () {
+        _playerRef = PlayerInstance.Instance.GetComponent<CharacterController2D>();
         StartCoroutine(PlaySequence());
 	}
 
     private IEnumerator PlaySequence() {
+        //suspend all input
+        _playerRef.Suspended = true;
+        //play every sequence object
         while (_playedIndex < steps.Length) {
             steps[_playedIndex].PlaySequenceObject();
             yield return new WaitForSeconds(steps[_playedIndex].sequenceDuration);
             _playedIndex++;
         }
-
+        //return control to the players
+        _playerRef.Suspended = false;
     }
 }
