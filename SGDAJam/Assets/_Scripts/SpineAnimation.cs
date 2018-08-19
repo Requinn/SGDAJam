6a0fe2b886d9edba2b534a68bd68de019cb25b0a
Michaelwolf95 <michaelwolf95@gmail.com>
2018-08-19 11:14:48 -0700

    Dragon Update

---
+++ SGDAJam/Assets/_Scripts/SpineAnimation.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Spine.Unity;
+using UnityEngine;
+
+public class SpineAnimation : StateMachineBehaviour
+{
+    public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        //SkeletonAnimation
+    }
+}
